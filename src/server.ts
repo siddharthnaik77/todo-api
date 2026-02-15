77a9bc3322ef727fa97822992b98a4b04f181727
Siddharth Naik <your.email@example.com>
2026-02-15 16:35:30 +0530

    cors issue solve

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import taskRoutes from "./routes/taskRoutes";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 // Middleware
 app.use(express.json());
